c713bdfd83ff550843ad0d3c1a47de5e0df3ca43
Gonav <gonav@MacBook-de-Victor-Gonzalez.local>
2018-12-20 18:42:21 +0100

    -

--- exercise1_svm.py
+++ exercise1_svm.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    import pylab as pl
 
     def generate_data_set1():
         # generate training data in the 2-d case
@@ -67,7 +66,42 @@ if __name__ == "__main__":
         return X_test, y_test
 
     def run_svm_dataset1():
+
+        ####
+        # Write here your SVM code and choose a linear kernel
+        # plot the graph with the support_vectors_
+        # Print on the console the number of correct predictions and the total of predictions
+        ####
+
         X1, y1, X2, y2 = generate_data_set1()
+        main_function(X1, y1, X2, y2, 'linear')
+
+
+    def run_svm_dataset2():
+
+        ####
+        # Write here your SVM code and choose a linear kernel with the best C parameter
+        # plot the graph with the support_vectors_
+        # print on the console the number of correct predictions and the total of predictions
+        ####
+
+        X1, y1, X2, y2 = generate_data_set2()
+        main_function(X1, y1, X2, y2, 'linear')
+
+
+    def run_svm_dataset3():
+
+        ####
+        # Write here your SVM code and use a gaussian kernel
+        # plot the graph with the support_vectors_
+        # print on the console the number of correct predictions and the total of predictions
+        ####
+
+        X1, y1, X2, y2 = generate_data_set3()
+        main_function(X1, y1, X2, y2, 'rbf')
+
+
+    def main_function(X1, y1, X2, y2, kernel_function):
         plt.plot(X1[:, 0], X1[:, 1], c='r', linestyle='None', marker='.')
         plt.plot(X2[:, 0], X2[:, 1], c='b', linestyle='None', marker='.')
         plt.xlabel('X1')
@@ -87,28 +121,28 @@ if __name__ == "__main__":
         plt.show()
 
         # Write here your SVM code and choose a linear kernel
-        svm = SVC(kernel='linear')
+        svm = SVC(kernel=kernel_function)
         svm.fit(X_train, y_train)
 
         # Plot the graph with the support_vectors_
         plt.plot(X_train[idx1, 0], X_train[idx1, 1], c='r', linestyle='None', marker='.')
         plt.plot(X_train[idx2, 0], X_train[idx2, 1], c='b', linestyle='None', marker='.')
-        plt.plot(X_train[svm.support_,0], X_train[svm.support_,1], c='g', linestyle='None', marker='*')
+        plt.plot(X_train[svm.support_, 0], X_train[svm.support_, 1], c='g', linestyle='None', marker='*')
         plt.xlabel('X1')
         plt.ylabel('X2')
         plt.title('Labeled Training set with support vectors')
         plt.show()
 
         # Plot decidecision boundaries
-        xmin, xmax = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
-        ymin, ymax = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
-        x, y = np.meshgrid(np.arange(xmin, xmax, 0.01),np.arange(ymin, ymax, 0.01))
+        xmin, xmax = X_train[:, 0].min() - 0.5, X_train[:, 0].max() + 0.5
+        ymin, ymax = X_train[:, 1].min() - 0.5, X_train[:, 1].max() + 0.5
+        x, y = np.meshgrid(np.arange(xmin, xmax, 0.01), np.arange(ymin, ymax, 0.01))
         z = svm.predict(np.c_[x.ravel(), y.ravel()])
         z = z.reshape(x.shape)
-        plt.contourf(x, y, z, alpha=0.2, colors=['blue','red'])
+        plt.contourf(x, y, z, alpha=0.2, colors=['blue', 'red'])
         plt.plot(X_train[idx1, 0], X_train[idx1, 1], c='r', linestyle='None', marker='.')
         plt.plot(X_train[idx2, 0], X_train[idx2, 1], c='b', linestyle='None', marker='.')
-        plt.plot(X_train[svm.support_,0], X_train[svm.support_,1], c='g', linestyle='None', marker='*')
+        plt.plot(X_train[svm.support_, 0], X_train[svm.support_, 1], c='g', linestyle='None', marker='*')
         plt.xlabel('X1')
         plt.ylabel('X2')
         plt.title('Labeled Training set with support vectors')
@@ -135,133 +169,23 @@ if __name__ == "__main__":
 
         # Print on the console the number of correct predictions and the total of predictions
         num_correct_predict = sum([x == y for x, y in zip(y_test, scores)])
-        print('Number of correct predictions: '+str(num_correct_predict))
-        print('Total number of predictions: '+str(len(y_test)))
-        accuracy = num_correct_predict/len(y_test)
-        print('Accuracy: '+str(accuracy))
-
-
-    def run_svm_dataset2():
-        X1, y1, X2, y2 = generate_data_set2()
-        X_train, y_train = split_train(X1, y1, X2, y2)
-        X_test, y_test = split_test(X1, y1, X2, y2)
-
-        #### 
-        # Write here your SVM code and choose a linear kernel with the best C pparameter
-        # plot the graph with the support_vectors_
-        # print on the console the number of correct predictions and the total of predictions
-        ####
-
-
-
-    def run_svm_dataset3():
-        X1, y1, X2, y2 = generate_data_set3()
-        X_train, y_train = split_train(X1, y1, X2, y2)
-        X_test, y_test = split_test(X1, y1, X2, y2)
-
-        #### 
-        # Write here your SVM code and use a gaussian kernel 
-        # plot the graph with the support_vectors_
-        # print on the console the number of correct predictions and the total of predictions
-        ####
-
-
+        print('Number of correct predictions: ' + str(num_correct_predict))
+        print('Total number of predictions: ' + str(len(y_test)))
+        accuracy = num_correct_predict / len(y_test)
+        print('Accuracy: ' + str(accuracy))
 
 #############################################################
 #############################################################
 #############################################################
 
-# EXECUTE SVM with THIS DATASETS      
+# EXECUTE SVM with THIS DATASETS
+    print('\033[1m'+'Results using a SVM with a linear kernel'+'\033[0m')
     run_svm_dataset1()   # data distribution 1
+    print('\n\033[1m'+'Results using a SVM with a linear kernel and the best parameter of C'+'\033[0m')
     run_svm_dataset2()   # data distribution 2
+    print('\n\033[1m'+'Results using a SVM with a gaussian kernel'+'\033[0m')
     run_svm_dataset3()   # data distribution 3
 
 #############################################################
 #############################################################
 #############################################################
-
-
-#import numpy as np
-#import matplotlib.pyplot as plt
-#from sklearn import svm, datasets
-#
-#
-#def make_meshgrid(x, y, h=.02):
-#   """Create a mesh of points to plot in
-#
-#   Parameters
-#   ----------
-#   x: data to base x-axis meshgrid on
-#   y: data to base y-axis meshgrid on
-#   h: stepsize for meshgrid, optional
-#
-#   Returns
-#   -------
-#   xx, yy : ndarray
-#   """
-#   x_min, x_max = x.min() - 1, x.max() + 1
-#   y_min, y_max = y.min() - 1, y.max() + 1
-#   xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-#                        np.arange(y_min, y_max, h))
-#   return xx, yy
-#
-#
-#def plot_contours(ax, clf, xx, yy, **params):
-#   """Plot the decision boundaries for a classifier.
-#
-#   Parameters
-#   ----------
-#   ax: matplotlib axes object
-#   clf: a classifier
-#   xx: meshgrid ndarray
-#   yy: meshgrid ndarray
-#   params: dictionary of params to pass to contourf, optional
-#   """
-#   Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-#   Z = Z.reshape(xx.shape)
-#   out = ax.contourf(xx, yy, Z, **params)
-#   return out
-#
-#
-## import some data to play with
-#iris = datasets.load_iris()
-## Take the first two features. We could avoid this by using a two-dim dataset
-#X = iris.data[:, :2]
-#y = iris.target
-#
-## we create an instance of SVM and fit out data. We do not scale our
-## data since we want to plot the support vectors
-#C = 1.0  # SVM regularization parameter
-#models = (svm.SVC(kernel='linear', C=C),
-#         svm.LinearSVC(C=C),
-#         svm.SVC(kernel='rbf', gamma=0.7, C=C),
-#         svm.SVC(kernel='poly', degree=3, C=C))
-#models = (clf.fit(X, y) for clf in models)
-#
-## title for the plots
-#titles = ('SVC with linear kernel',
-#         'LinearSVC (linear kernel)',
-#         'SVC with RBF kernel',
-#         'SVC with polynomial (degree 3) kernel')
-#
-## Set-up 2x2 grid for plotting.
-#fig, sub = plt.subplots(2, 2)
-#plt.subplots_adjust(wspace=0.4, hspace=0.4)
-#
-#X0, X1 = X[:, 0], X[:, 1]
-#xx, yy = make_meshgrid(X0, X1)
-#
-#for clf, title, ax in zip(models, titles, sub.flatten()):
-#   plot_contours(ax, clf, xx, yy,
-#                 cmap=plt.cm.coolwarm, alpha=0.8)
-#   ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-#   ax.set_xlim(xx.min(), xx.max())
-#   ax.set_ylim(yy.min(), yy.max())
-#   ax.set_xlabel('Sepal length')
-#   ax.set_ylabel('Sepal width')
-#   ax.set_xticks(())
-#   ax.set_yticks(())
-#   ax.set_title(title)
-#
-#plt.show()
-#
